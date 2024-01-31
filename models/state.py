cce24135846a70515523e43411194a38d2bbd5e7
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-01-31 16:37:20 +0000

    fixed classes for task 6

--- models/state.py
+++ models/state.py
@@ -4,15 +4,16 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from os import getenv
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(60), nullable=False)
+    name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="delete")
 
-    if getenv("HBNB_MYSQL_DB") != "db":
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """ getter for cities """
