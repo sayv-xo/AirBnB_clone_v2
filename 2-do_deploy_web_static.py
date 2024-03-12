f3c55d2ee0ac286a027a0901b7f1fc738e9eea7a
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-03-12 11:10:53 +0000

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,21 +11,20 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return False
     filename = archive_path.split("/")[-1]
-    no_ext = filename.split(".")[0]
-    full_path = "/data/web_static/releases/" + no_ext
+    name = filename.split(".")[0]
     try:
-        # upload archive file to tmp dir of server
         put(archive_path, '/tmp/')
-        # create directories
-        run("mkdir -p /data/web_static/releases/{}".format(no_ext))
-        # uncompress archive file
-        run("tar -xzf /tmp/{} -C {}/".format(filename, full_path))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+            format(filename, name))
         run("rm /tmp/{}".format(filename))
-        run("mv {}/web_static/* {}/".format(full_path))
-        run("rm -rf {}/web_static".format(full_path))
+        run("mv /data/web_static/releases/{}/web_static/* "
+            "/data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".
+            format(name))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(full_path))
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+            format(name))
         print("New version deployed!")
-        return True
     except Exception:
         return False
