b33b08974ab373dfe5534c1ae603449682db2147
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-03-23 16:08:45 +0000

    simple web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" start a flask web app """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """ return a string on query """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
