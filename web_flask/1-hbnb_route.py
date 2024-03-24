a430529aa871cb7e2f21962abfbaf4334c599bbd
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-03-24 12:12:24 +0000

    app routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" start a flask web app """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """ return a string on query """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ return a string on query """
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" start a flask web app """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    """ return a string on query """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """ return a string on query """
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """ return string on query """
+    return "C " + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
